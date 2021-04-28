1bb834019e219346d448d353802d25ae1f3e2a4d
XYlearn <1014355965@qq.com>
2021-04-28 20:55:23 +0800

    fix: change the dist image distribution, build in temporary folder, and enlarge the size

--- scripts/disk-build.sh
+++ scripts/disk-build.sh
@@ -1,14 +1,32 @@
-#!bash
+#!/bin/bash
 
-RELEASE=full
+RELEASE=stretch
 
-curl -s https://raw.githubusercontent.com/google/syzkaller/master/tools/create-image.sh \
-  | sed "s/syzkaller/XYlearn/" 
-  | bash -s -- --distribution $RELEASE --seek 4096
-exit
-# Move to images
 SCRIPT_DIR=$(dirname $0)
 ROOT="$SCRIPT_DIR/.."
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+TEMPDIR=`mktemp -d`
+
+pushd $TEMPDIR
+
+curl -s https://raw.githubusercontent.com/google/syzkaller/master/tools/create-image.sh \
+  | sed "s/syzkaller/XYlearn/" \
+  | bash -s -- --distribution $RELEASE --seek 8096 # leave 8 GB
+exit
+
+# Move to images
 sudo mv $RELEASE.img $ROOT/images/disk.img
 sudo mv $RELEASE.id_rsa $ROOT/images/ssh.id_rsa
 sudo mv $RELEASE.id_rsa.pub $ROOT/images/ssh.id_rsa.pub
+
+popd
+
+rm -r $TEMPDIR
