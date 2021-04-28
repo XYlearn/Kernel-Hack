670925fdefb2a223b2283bcc55ebf0048ea61fb2
XYlearn <1014355965@qq.com>
2021-04-28 20:53:44 +0800

    fix: make this script usable

--- scripts/kernel-module-install.sh
+++ scripts/kernel-module-install.sh
@@ -3,12 +3,16 @@ ROOT="$(dirname $0)/.."
 IMAGE_DIR=$ROOT/images
 
 if [[ $# != 1 ]]; then
-  echo "Usage: $0 IDENTIFIER TARGETDIR"
+  echo "Usage: $0 IDENTIFIER"
   exit
 fi
 
 IDEN=$1
 SOURCE=$ROOT/images/modules-$IDEN/lib
-echo $SOURCE
-# ssh -i $ROOT/images/ssh.id_rsa -p 10021 -o "StrictHostKeyChecking no" root@localhost "if [[ -e $TARGETDIR ]] rm -r $TARGETDIR"
-# scp -i $ROOT/images/ssh.id_rsa -P 10021 -o "StrictHostKeyChecking no" -r $SOURCE root@localhost:/
+
+if [[ ! -d $SOURCE ]];then
+  echo "Build kernel for $IDEN first"
+  exit -1
+fi
+
+scp -i $ROOT/images/ssh.id_rsa -P 10021 -o "StrictHostKeyChecking no" -prq $SOURCE root@localhost:/
